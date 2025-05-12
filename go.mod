825e0499c74449f09edc6fd9f56cea4d14676a3b
kevwasonga <kevinwasonga116@gmail.com>
2025-05-12 08:21:46 +0300

    feat: set up a simple http connection to be used in testing kubernetes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ku
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("server open on port 8080")
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hello Kubernetes")
+}
